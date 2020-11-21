64358ac28735d9483c25be833a6f4ab07fdb5143
NikitaInozemtsev <70565510+NikitaInozemtsev@users.noreply.github.com>
2020-11-21 21:56:51 +0300

    px
    px

---
+++ Pixels/target/generated/cxf/serv/service/InsertResponse.java
@@ -0,0 +1,54 @@
+
+package serv.service;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for insertResponse complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="insertResponse"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="Serv" type="{http://www.w3.org/2001/XMLSchema}boolean"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "insertResponse", propOrder = {
+    "serv"
+})
+public class InsertResponse {
+
+    @XmlElement(name = "Serv")
+    protected boolean serv;
+
+    /**
+     * Gets the value of the serv property.
+     * 
+     */
+    public boolean isServ() {
+        return serv;
+    }
+
+    /**
+     * Sets the value of the serv property.
+     * 
+     */
+    public void setServ(boolean value) {
+        this.serv = value;
+    }
+
+}
---
+++ Pixels/target/generated/cxf/serv/service/UpdateResponse.java
@@ -0,0 +1,60 @@
+
+package serv.service;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for updateResponse complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="updateResponse"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="Serv" type="{http://www.w3.org/2001/XMLSchema}base64Binary" minOccurs="0"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "updateResponse", propOrder = {
+    "serv"
+})
+public class UpdateResponse {
+
+    @XmlElement(name = "Serv")
+    protected byte[] serv;
+
+    /**
+     * Gets the value of the serv property.
+     * 
+     * @return
+     *     possible object is
+     *     byte[]
+     */
+    public byte[] getServ() {
+        return serv;
+    }
+
+    /**
+     * Sets the value of the serv property.
+     * 
+     * @param value
+     *     allowed object is
+     *     byte[]
+     */
+    public void setServ(byte[] value) {
+        this.serv = value;
+    }
+
+}
